e35ec60c510bc63f4fbd6c8f0c958fef26dd23f4
Wanderson Ramos <wandersonramos740@gmail.com>
2023-11-21 22:20:21 -0300

    feat: create UrlController

---
+++ src/main/java/com/example/demo/controller/UrlController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.dto.UrlDTO;
+import com.example.demo.service.UrlService;
+
+@RestController
+public class UrlController {
+
+   private final UrlService urlService;
+
+    public UrlController(UrlService urlService){
+        this.urlService = urlService;
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public UrlDTO tinyUrl(@RequestBody UrlDTO urlDTO){
+        return this.urlService.tinyUrl(urlDTO);
+    }
+
+    @GetMapping("/{url}")
+    @ResponseStatus(code = HttpStatus.TEMPORARY_REDIRECT)
+    public ModelAndView findByShortURL(@PathVariable String url) {
+        return this.urlService.findByShortURL(url);
+    }
+}
